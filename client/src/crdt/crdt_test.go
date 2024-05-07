48168f4f093522bb3932cd1e0d11e3e41a6fc5f7
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2024-05-07 23:39:05 +0100

    message protobuf for crdt

---
+++ client/src/crdt/crdt_test.go
@@ -0,0 +1,48 @@
+package main
+
+import "crdt"
+
+func main() {
+	data := &pb.Message{
+		Type: pb.Type_register,
+		Msg: &pb.Message_M1{
+			M1: &pb.RegisterLoginFormat{
+				UserName: "UserName",
+				Password: "Password",
+			},
+		},
+	}
+
+	out, err := proto.Marshal(data)
+
+	if err != nil {
+		panic("lmao")
+	}
+
+	//establish connection
+	connection, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+	if err != nil {
+		panic(err)
+	}
+
+	///send some data
+	_, err = connection.Write(out)
+
+	buffer := make([]byte, 1024)
+
+	connection.Read(buffer)
+
+    data_out := &pb.Message{}
+
+    proto.Unmarshal(buffer, data_out)
+
+    fmt.Printf("buffer: %v\n", buffer)
+
+    fmt.Println(data_out.Type)
+
+    x := data_out.GetM5()
+
+    fmt.Printf("x.Status: %v\n", x.Status)
+
+	defer connection.Close()
+}
